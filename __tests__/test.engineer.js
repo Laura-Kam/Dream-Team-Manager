1711ec3e6886f11dc27e6f925efd59337f314fdf
Laura Kambanis <laurakambanis@Lauras-MacBook-Air-3.local>
2022-09-10 14:24:12 +0100

    tests added

--- __tests__/test.engineer.js
+++ __tests__/test.engineer.js
@@ -1,9 +1,31 @@
-const Employee = require("../lib/engineer");
+const Engineer = require("../lib/engineer");
 
-describe("Engineer", () => {
-  test("should return an object containing a 'github' property", () => {
-    const obj = new Employee();
+describe("Constructor name", () => {
+  test("Can set name via constructor argument", () => {
+    const testName1 = "Laura";
 
-    expect("github" in obj).toEqual(true);
+    const element1 = new Engineer(testName);
+
+    expect(element1.name).toBe(testName1);
+  });
+});
+
+describe("Constructor id", () => {
+  test("Can set id via constructor argument", () => {
+    const testId = 1;
+
+    const element = new Employee("laura-k@yahoo.co.uk", testId);
+
+    expect(element.id).toBe(testId);
+  });
+});
+
+describe("Constructor id", () => {
+  test("Can set id via constructor argument", () => {
+    const testId = "1";
+
+    const element = new Employee("laura-k@yahoo.co.uk", testId);
+
+    expect(element.id).toBe(testId);
   });
 });
